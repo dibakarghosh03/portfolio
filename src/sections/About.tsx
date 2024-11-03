9f371319f49bfbdffc14a3d10c59da970f220d1f
Dibakar Ghosh <ghoshdibakar81@gmail.com>
2024-11-03 19:19:38 +0530

    created About section

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,178 @@
+import StarIcon from "@/assets/icons/star.svg";
+import Image from "next/image";
+import bookImage from "@/assets/images/book-cover.png";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import NextIcon from "@/assets/icons/next-js-svgrepo-com.svg";
+import ToolboxItems from "@/components/ToolboxItems";
+import MapImage from "@/assets/images/map-image.png";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+
+const toolboxItems = [
+  {
+    title: "JavaScript",
+    iconType: JavascriptIcon,
+  },
+  {
+    title: "HTML5",
+    iconType: HTMLIcon,
+  },
+  {
+    title: "CSS3",
+    iconType: CssIcon,
+  },
+  {
+    title: "React",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Github",
+    iconType: GithubIcon,
+  },
+  {
+    title: "Next JS",
+    iconType: NextIcon,
+  },
+];
+
+const hobbies = [
+  {
+    title: "Movies",
+    emoji: "🎦",
+    left: "40%",
+    top: "58%",
+  },
+  {
+    title: "Gaming",
+    emoji: "🎮",
+    left: "35%",
+    top: "20%",
+  },
+  {
+    title: "Music",
+    emoji: "🎵",
+    left: "68%",
+    top: "5%",
+  },
+  {
+    title: "Reading",
+    emoji: "📚",
+    left: "1%",
+    top: "50%",
+  },
+  {
+    title: "Coding",
+    emoji: "🧑🏻‍💻",
+    left: "56%",
+    top: "34%",
+  },
+  {
+    title: "Cricket",
+    emoji: "🏏",
+    left: "1%",
+    top: "10%",
+  },
+];
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="py-20">
+      <div className="container">
+        <div className="flex justify-center">
+          <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 text-transparent bg-clip-text">
+            About Me
+          </p>
+        </div>
+        <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">
+          A Glimpse Into My World
+        </h2>
+        <p className="text-center text-white/60 mt-4 md:text-lg lg:text-xl max-w-md mx-auto">
+          Learn more about who I am, what I do, and what inspires me.
+        </p>
+        <div className="mt-20 flex flex-col gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
+            <div className="bg-gray-800 rounded-3xl relative overflow-hidden border-2 border-white/20 p-6 h-[320px] md:col-span-2">
+              <div className="flex flex-col">
+                <div className="inline-flex gap-2 items-center">
+                  <StarIcon className="size-9 text-emerald-300" />
+                  <h3 className="font-serif text-3xl">My Reads</h3>
+                </div>
+                <p className="text-sm md:text-base text-white/60 mt-2">
+                  Explore the books shaping my perspectives.
+                </p>
+              </div>
+              <div className="w-40 mx-auto mt-8">
+                <Image src={bookImage} alt="Book Cover" />
+              </div>
+            </div>
+            <div className="bg-gray-800 rounded-3xl relative overflow-hidden border-2 border-white/20 h-[320px] md:col-span-3">
+              <div className="flex flex-col px-6 pt-6">
+                <div className="inline-flex gap-2 items-center">
+                  <StarIcon className="size-9 text-emerald-300" />
+                  <h3 className="font-serif text-3xl">My Toolbox</h3>
+                </div>
+                <p className="text-sm md:text-base text-white/60 mt-2">
+                  Explore the technologies and tools I use to create exceptional
+                  digital experiences.
+                </p>
+              </div>
+              <ToolboxItems toolboxItems={toolboxItems} className="mt-6" />
+              <ToolboxItems
+                toolboxItems={toolboxItems}
+                className="mt-6"
+                itemsWrapperClassName="-translate-x-1/2"
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-5 gap-8">
+            <div className="bg-gray-800 rounded-3xl relative overflow-hidden border-2 border-white/20 h-[320px] flex flex-col md:col-span-3">
+              <div className="px-6 py-6">
+                <div className="inline-flex gap-2 items-center">
+                  <StarIcon className="size-9 text-emerald-300" />
+                  <h3 className="font-serif text-3xl">Beyond the Code</h3>
+                </div>
+                <p className="text-sm md:text-base text-white/60 mt-2">
+                  Explore my interests and hobbies beyond the digital realm.
+                </p>
+              </div>
+              <div className="relative flex-1">
+                {hobbies.map((hobby) => (
+                  <div
+                    key={hobby.title}
+                    className="inline-flex items-center gap-2 px-6 bg-gradient-to-r from-emerald-300 to-sky-400 rounded-full py-1.5 absolute"
+                    style={{
+                      left: hobby.left,
+                      top: hobby.top,
+                    }}
+                  >
+                    <span className="text-gray-950 font-medium">
+                      {hobby.title}
+                    </span>
+                    <span>{hobby.emoji}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+            <div className="bg-gray-800 rounded-3xl relative overflow-hidden border-2 border-white/20 h-[320px] md:col-span-2">
+              <Image
+                src={MapImage}
+                alt="Map Image"
+                className="h-full w-full object-cover z-0"
+              />
+              <div className="absolute z-10 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 size-20 rounded-full bg-gradient-to-r from-emerald-300 to-sky-400 border-2 border-sky-500">
+                <Image
+                  src={smileMemoji}
+                  alt="Smile Memoji"
+                  className="size-20"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
